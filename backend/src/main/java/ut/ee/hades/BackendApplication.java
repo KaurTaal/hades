77d078ef7527764252619a35a45e401073fe6278
kaur.taal <kaur.taal@helmes.com>
2023-04-27 00:09:54 +0300

    backend deploy setup

--- backend/src/main/java/ut/ee/hades/BackendApplication.java
+++ backend/src/main/java/ut/ee/hades/BackendApplication.java
@@ -1,12 +1,15 @@
 package ut.ee.hades;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
